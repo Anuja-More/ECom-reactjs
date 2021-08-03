2f4aedffa58e332c31f8424944037d9c7e37f8ef
Anuja Ajit More <80687691+Anuja-More@users.noreply.github.com>
2021-08-03 15:12:16 +0530

    Initial commit

---
+++ src/data/users.js
@@ -0,0 +1,7 @@
+const users = [
+  { username: "AnujaMore", password: "123456" },
+  { username: "Dikshu", password: "123456" },
+  { username: "sanji", password: "123456" }
+];
+
+export default users;
